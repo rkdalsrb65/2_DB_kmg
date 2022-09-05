6f536a4c31ee54b32f0fe3ebb4f413a5cbfcbc0e
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-05 11:49:29 +0900

    DML

--- 05_DML.sql
+++ 05_DML.sql
@@ -8,6 +8,7 @@
 CREATE TABLE EMPLOYEE2 AS SELECT * FROM EMPLOYEE;
 CREATE TABLE DEPARTMENT2 AS SELECT * FROM DEPARTMENT;
 
+DROP TABLE DEPARTMENT2; -- 잘못 롤백했을 때
 
 SELECT * FROM EMPLOYEE2;
 SELECT * FROM DEPARTMENT2;
@@ -118,18 +119,23 @@ SELECT EMP_NAME, BONUS FROM EMPLOYEE2;
 ---------------------------------------
 
 -- * 조건절을 설정하지 않고 UPDATE 구문 실행 시 모든 행의 컬럼 값 변경.
+SELECT * FROM DEPARTMENT2;
 
+UPDATE DEPARTMENT2 SET
+DEPT_TITLE = '기술연구팀';
 
-
+ROLLBACK;
 
 ---------------------------------------
-
-
 
 -- * 여러 컬럼을 한번에 수정할 시 콤마(,)로 컬럼을 구분하면됨.
 -- D9 / 전략기획팀  -> D0 / 전략기획2팀으로 수정
 
+UPDATE DEPARTMENT2 SET
+DEPT_ID = 'D0', DEPT_TITLE = '전략기획2팀' -- WHERE절 직전 컬럼 작성 후 콤마(,) X
+WHERE DEPT_ID = 'D9' AND DEPT_TITLE = '전략기획팀';
 
+SELECT * FROM DEPARTMENT2;
 
 ---------------------------------------
 
@@ -144,31 +150,22 @@ SELECT EMP_NAME, BONUS FROM EMPLOYEE2;
 -- 급여와 보너스율을 유재식 사원과 동일하게 변경해 주기로 했다.
 -- 이를 반영하는 UPDATE문을 작성하시오.
 
-   
----------------------------------------
+-- 유재식 급여
+SELECT SALARY FROM EMPLOYEE2 WHERE EMP_NAME = '유재식';
 
+-- 유재식 보너스
+SELECT BONUS FROM EMPLOYEE2 WHERE EMP_NAME = '유재식';
 
--- * 다중행 다중열 서브쿼리를 이용한 UPDATE문
+-- 방명수 급여, 보너스 수정
 
--- EMPLOYEE2 테이블에서
--- 방명수 사원의 급여 인상 소식을 전해들은 다른 멤버들이
--- 단체로 파업을 진행했다.
--- 노옹철, 전형돈, 정중하, 하동운 사원의 급여와 보너스를
--- 유재식 사원의 급여와 보너스와 같게 변경하는 UPDATE문을 작성하시오.
+UPDATE EMPLOYEE2 SET
+SALARY = (SELECT SALARY FROM EMPLOYEE2 WHERE EMP_NAME = '유재식'),
+BONUS = (SELECT BONUS FROM EMPLOYEE2 WHERE EMP_NAME = '유재식') -- WHERE절 직전 컬럼 작성 후 콤마(,) X
+WHERE EMP_NAME = '방명수';
 
-
-
-
-
-
--- EMP_SALARY테이블에서 아시아지역에 근무하는 직원의 보너스를 0.3으로 변경
-
-
--- 1) 아시아 지역에 근무하는 직원
-
-
--- 2) 아시아 지역 근무 직원 보너스 0.3으로 변경
-
+SELECT EMP_NAME, SALARY, BONUS
+FROM EMPLOYEE2
+WHERE EMP_NAME IN('유재식','방명수');
 
 --------------------------------------------------------------------------------------------------------------------
 
@@ -184,14 +181,17 @@ AS SELECT * FROM EMPLOYEE;
 CREATE TABLE EMP_M02
 AS SELECT * FROM EMPLOYEE
    WHERE JOB_CODE = 'J4';
-   
+
+SELECT * FROM EMP_M01;
+SELECT * FROM EMP_M02;
+
 INSERT INTO EMP_M02
 VALUES (999, '곽두원', '561016-1234567', 'kwack_dw@kh.or.kr',
         '01011112222', 'D9', 'J4', 'S1', 9000000, 0.5, NULL,
         SYSDATE, NULL, DEFAULT);
        
-SELECT * FROM EMP_M01; 
-SELECT * FROM EMP_M02;
+SELECT * FROM EMP_M01; -- 23명
+SELECT * FROM EMP_M02; -- 5명(기존4명 + 신규 1명)
 
 UPDATE EMP_M02 SET SALARY = 0;
 
@@ -220,7 +220,7 @@ INSERT VALUES (EMP_M02.EMP_ID, EMP_M02.EMP_NAME, EMP_M02.EMP_NO,
                EMP_M02.BONUS, EMP_M02.MANAGER_ID, EMP_M02.HIRE_DATE,
                EMP_M02.ENT_DATE, EMP_M02.ENT_YN);
 
-SELECT * FROM EMP_M01; 
+SELECT * FROM EMP_M01; -- 24명
 
 
 --------------------------------------------------------------------------------------------------------------------
@@ -236,12 +236,26 @@ COMMIT;
 
 -- EMPLOYEE2 테이블에서 '장채현'사원 정보 조회
 
+SELECT * FROM EMPLOYEE2
+WHERE EMP_NAME = '장채현';
+
 -- EMPLOYEE2 테이블에서 이름이 '장채현'인 사원 정보 삭제
 
+DELETE FROM EMPLOYEE2
+WHERE EMP_NAME = '장채현';
+
 -- 삭제 확인
+SELECT * FROM EMPLOYEE2
+WHERE EMP_NAME = '장채현'; --> 조회 결과 없음
 
+ROLLBACK;
 -- EMPLOYEE2 테이블 전체 삭제
+SELECT * FROM EMPLOYEE2;
 
+DELETE FROM DEMPLOYEE2; --> WHERE 미작성 시 전체 삭제
+--> 24행 삭제
+
+ROLLBACK;
 
 ---------------------------------------------------------------------------------------------
 
@@ -278,3 +292,6 @@ SELECT * FROM EMPLOYEE3;
 ROLLBACK;
 -- 롤백 후 복구 확인 -> 복구 안됨을 확인!
 SELECT * FROM EMPLOYEE3;
+
+-- DELETE : 휴지통 버리기
+-- TRUNCATE : 완전 삭제
