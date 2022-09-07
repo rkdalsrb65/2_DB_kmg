42e825824db3eedb268e712830de4c7ce1a74262
rkdalsrb65 <rkdalsrb65@naver.com>
2022-09-07 11:44:07 +0900

    TCL

--- 06_TCL.sql
+++ 06_TCL.sql
@@ -7,9 +7,11 @@
 ​
 /* TRANSACTION이란?
 - 데이터베이스의 논리적 연산 단위
+
 - 데이터 변경 사항을 묶어 하나의 트랜잭션에 담아 처리함.
 ​
-- 트랜잭션의 대상이 되는 데이터 변경 사항 : INSERT, UPDATE, DELETE (DML)
+- 트랜잭션의 대상이 되는 데이터 변경 사항 : INSERT, UPDATE, DELETE (DML), MERGE
+
 EX) INSERT 수행 --------------------------------> DB 반영(X)
  
     INSERT 수행 --> 트랜잭션에 추가 --> COMMIT --> DB 반영(O)
@@ -20,7 +22,7 @@ EX) INSERT 수행 --------------------------------> DB 반영(X)
    1) COMMIT : 메모리 버퍼(트랜잭션)에 임시 저장된 데이터 변경 사항을 DB에 반영
    
    2) ROLLBACK : 메모리 버퍼(트랜잭션)에 임시 저장된 데이터 변경 사항을 삭제하고
-                마지막 COMMIT 상태로 돌아감.
+                마지막 COMMIT 상태로 돌아감. (DB에 변경 내용 반영 X)
                
    3) SAVEPOINT : 메모리 버퍼(트랜잭션)에 저장 지점을 정의하여
                   ROLLBACK 수행 시 전체 작업을 삭제하는 것이 아닌
@@ -35,3 +37,67 @@ EX) INSERT 수행 --------------------------------> DB 반영(X)
    ROLLBACK TO 포인트명1; -- 포인트1 지점 까지 데이터 변경사항 삭제
 ​
 */
+
+
+SELECT * FROM DEPARTMENT2;
+
+-- 새로운 데이터 INSERT
+
+INSERT INTO DEPARTMENT2 VALUES('T1', '개발1팀', 'L2');
+INSERT INTO DEPARTMENT2 VALUES('T2', '개발2팀', 'L2');
+INSERT INTO DEPARTMENT2 VALUES('T3', '개발3팀', 'L2');
+
+-- INSERT 확인
+SELECT * FROM DEPARTMENT2;
+--> DB에 반영된 것 처럼 보이지만
+-- SQL 수행 시 트랜잭션 내용도 포함해서 수행된다.
+-- (실제로 아직 DB 반영 X)
+
+-- ROLLBACK 후 확인
+ROLLBACK;
+SELECT * FROM DEPARTMENT2;
+
+-- COMMIT 후 ROLLBACK이 되는지 확인
+INSERT INTO DEPARTMENT2 VALUES('T1', '개발1팀', 'L2');
+INSERT INTO DEPARTMENT2 VALUES('T2', '개발2팀', 'L2');
+INSERT INTO DEPARTMENT2 VALUES('T3', '개발3팀', 'L2');
+
+COMMIT;
+
+SELECT * FROM DEPARTMENT2;
+
+ROLLBACK;
+
+SELECT * FROM DEPARTMENT2; --> 롤백 안됨!
+
+------------------------------------------------------
+
+-- SAVEPOINT 확인
+
+INSERT INTO DEPARTMENT2 VALUES('T4', '개발4팀', 'L2');
+SAVEPOINT SP1; -- SAVEPOINT 지정
+
+INSERT INTO DEPARTMENT2 VALUES('T5', '개발5팀', 'L2');
+SAVEPOINT SP2; -- SAVEPOINT 지정
+
+INSERT INTO DEPARTMENT2 VALUES('T6', '개발6팀', 'L2');
+
+SELECT * FROM DEPARTMENT2;
+
+-- SP2 지점까지 롤백
+ROLLBACK TO SP2;
+SELECT * FROM DEPARTMENT2; -- 개발6팀만 롤백
+
+ROLLBACK TO SP1;
+SELECT * FROM DEPARTMENT2; -- 개발5팀만 롤백
+
+-- 롤백 수행
+ROLLBACK;
+SELECT * FROM DEPARTMENT2; -- 개발4팀 롤백
+
+
+
+
+
+
+
